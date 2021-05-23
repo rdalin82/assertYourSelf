769134e7e96748897c2483c03d2bc43900c2ad90
rdalin82 <rdalin@benchprep.com>
2021-05-23 15:26:25 -0500

    Initial and add a couple tests

--- assertYourSelfTests/assertYourSelfTests.swift
+++ assertYourSelfTests/assertYourSelfTests.swift
@@ -6,28 +6,16 @@
 //
 
 import XCTest
-@testable import assertYourSelf
+//@testable import assertYourSelf
 
 class assertYourSelfTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_pass() {
+        let actual = "actual"
+        XCTAssertEqual(actual, "actual")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_floatingPointFixed() {
+        let result = 0.1 + 0.2
+        XCTAssertEqual(result, 0.3, accuracy: 0.001)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
